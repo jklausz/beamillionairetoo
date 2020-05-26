c937865ac5488ec21167120fd6b609278eff733b
Junászka Klaudia <klausz8801@gmail.com>
2020-05-26 21:01:53 +0200

    questionsjs jav

--- questions.js
+++ questions.js
@@ -1,32 +1,48 @@
-const questionArrEasy = [
-    {
-        question: 'Milyen színű a hó?',
-        answers: ["kek", "zold", "piros", "fehér"],
-        answer1: 'kék',
-        answer2: 'zöld',
-        answer3: 'piros',
-        answer4: 'fehér',
-        correct: 'fehér'
-    },
-    {
-        question: 'Hány lába van a kutyának?',
-        answers: ['1', '5', '18', '4'],
-        answer1: '1',
-        answer2: '5',
-        answer3: '18',
-        answer4: '4',
-        correct: '4'
-    },
-    {
-        question: 'Mi Magyarország fővárosa?',
-        answers: ['Bukarest', 'Szeged', 'Róma', 'Budapest'],
-        answer1: 'Bukarest',
-        answer2: 'Szeged',
-        answer3: 'Róma',
-        answer4: 'Budapest',
-        correct: 'Budapest'
+const questionsEasy = require('./questionEasy');
+
+let actualQuestion = null;
+let askedQuestions = [];
+
+function askQuestion() {
+    //benne van-e mar a kerdes az askedQuestions-be?
+    //nem -> akkor kerdezzuk es rakjuk bele
+    //igen -> akkor kerdezzunk masikat
+    //hozzadni a kerdes indexet askedQuestions
+    //actualQuestion =  random question
+    //return random question
+}
+
+function checkAnswer(userAnswer) {
+    if(userAnswer === actualQuestion.correct) {
+        return true
+    } else {
+        false
     }
-];
+}
+
+[
+    {
+        "question": "Milyen színű a hó?",
+        "answers": [
+            "kek",
+            "zold",
+            "piros",
+            "fehér"
+        ],
+        "correct": "fehér"
+    },
+    {
+        "question": "Hány lába van a kutyának?",
+        "answers": ["1", "5", "18", "4"],
+        "correct": "4"
+    },
+    {
+        "question": "Mi Magyarország fővárosa?",
+        "answers": ["Bukarest", "Szeged", "Róma", "Budapest"],
+        "correct": "Budapest"
+    }
+]
+
 
 module.exports = {
     questionArrEasy: questionArrEasy
