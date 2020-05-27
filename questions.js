aa9a12f5be483a8d0d5e177da0e9265e082ac5a1
Junászka Klaudia <klausz8801@gmail.com>
2020-05-27 20:58:41 +0200

    questionsjs fix

--- questions.js
+++ questions.js
@@ -1,9 +1,22 @@
+// question.js
+
 const questionsEasy = require('./questionEasy.json');
 
 let actualQuestion = null;
 let askedQuestions = [];
+let currentQuestionCount = 0;
 
-function askQuestion() {
+function getCurrentQuestionCount() {
+    return currentQuestionCount;
+}
+
+function askQuestion(questionIndex = 0) {
+    //questionArrEasy[0]: az első kérdés a jsonból
+    console.log(questionsEasy[questionIndex].question);
+    console.log("a " + questionsEasy[questionIndex].answers[0].answer);
+    console.log("b " + questionsEasy[questionIndex].answers[1].answer);
+    console.log("c " + questionsEasy[questionIndex].answers[2].answer);
+    console.log("d " + questionsEasy[questionIndex].answers[3].answer);
     //benne van-e mar a kerdes az askedQuestions-be?
     //nem -> akkor kerdezzuk es rakjuk bele
     //igen -> akkor kerdezzunk masikat
@@ -12,38 +25,33 @@ function askQuestion() {
     //return random question
 }
 
-function checkAnswer(userAnswer) {
-    if (userAnswer === actualQuestion.correct) {
-        return true
+function checkAnswer(key)
+ {
+    console.log("a " + key + " választ választottad")
+    // kikeresi a nulladik kérdés válaszaiból
+    // azt a választ, amelyiknek a betűjele megegyezik a lenyomott billentyűvel
+    // const givenAnswer = questionArrEasy[0].answers.find(answer => answer.mark === key)
+    let givenAnswer = null
+    for (let i = 0; i < questionsEasy[currentQuestionCount].answers.length; i++) {
+        if (questionsEasy[currentQuestionCount].answers[i].mark === key) {
+            givenAnswer = questionsEasy[currentQuestionCount].answers[i];
+        }
+    }
+
+    //helyes válasz?
+
+    if (givenAnswer.correct) {
+        console.log("helyes");
+        currentQuestionCount++;
+        askQuestion(currentQuestionCount);
     } else {
-        false
+        console.log("helytelen");
     }
 }
 
-const questionArrEasy = [
-    {
-        "question": "Milyen színű a hó?",
-        "answers": [
-            "kek",
-            "zold",
-            "piros",
-            "fehér"
-        ],
-        "correct": "fehér"
-    },
-    {
-        "question": "Hány lába van a kutyának?",
-        "answers": ["1", "5", "18", "4"],
-        "correct": "4"
-    },
-    {
-        "question": "Mi Magyarország fővárosa?",
-        "answers": ["Bukarest", "Szeged", "Róma", "Budapest"],
-        "correct": "Budapest"
-    }
-]
-
-
 module.exports = {
-    questionArrEasy: questionArrEasy
+    askQuestion: askQuestion,
+    getCurrentQuestionCount: getCurrentQuestionCount,
+    checkAnswer: checkAnswer
 };
+
